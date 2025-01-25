e18b8c6feab12740e44e27ab784ea7eed3e61afd
StrangeQuark <zacharyzirkle@hotmail.com>
2025-01-25 09:58:19 -0600

    Completely refactor UserRegistrationForm

--- src/components/UserRegisterForm.js
+++ src/components/UserRegisterForm.js
@@ -7,56 +7,72 @@ import { FaCheckCircle } from "react-icons/fa";
 
 
 const UserRegisterForm = () => {
-  const[username, setUsername] = useState()
+  const[username, setUsername] = useState('')
   const[isUsernameValid, setIsUsernameValid] = useState(true)
 
+  const[email, setEmail] = useState('')
   const[isEmailValid, setIsEmailValid] = useState(true)
 
+  const[password, setPassword] = useState('')
+  const[isPasswordValid, setIsPasswordValid] = useState(true)
+
+  const[confirmPassword, setConfirmPassword] = useState('')
+  const[isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(true)
+
   const[isSubmitted, setIsSubmitted] = useState(false)
+  const[isSuccess, setIsSuccess] = useState(false)
 
-  function registrationHandler(username, password, email, confirmPassword) {
-
+  function registrationHandler() {
     setIsSubmitted(true)
 
-    if(username === '') {
-      setIsUsernameValid(false)
-      return
-    }
+    setIsUsernameValid(username !== '')
+    setIsEmailValid(email !== '')
+    setIsPasswordValid(password !== '')
+    setIsConfirmPasswordValid(confirmPassword !== '' && confirmPassword === password)
+
+    const isFormValid =
+      username !== '' &&
+      email !== '' &&
+      password !== '' &&
+      confirmPassword !== '' &&
+      confirmPassword === password;
+
+    if (!isFormValid)
+      return;
 
     var registerJSON = {"username": username, "password": password, "email": email}
 
     fetch('http://localhost:6001/auth/register', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(registerJSON)
-    }).then(response => response.json().then(
-      (data) => {
-        if(response.status === 403) {
-          //error out
-          return
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(registerJSON),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          if (response.status === 409) {
+            const data = await response.json().catch(() => ({}));
+            if (data.errorCode === 410) {
+              setIsUsernameValid(false);
+            } else if (data.errorCode === 401) {
+              setIsEmailValid(false);
+            }
+          }
+        } else {
+          const data = await response.json();
+          setIsSuccess(true);
         }
-
-        if(response.status === 409) {
-          //Custom error codes: 410 for username error, 401 for email error
-          data.errorCode === 410 ? setIsUsernameValid(false) : setIsEmailValid(false)
-          return
-        }
-
-        var requestSuccessDiv = document.getElementById('request-success-div')
-        //Show user a message that a verification email has been sent to their account
-        document.getElementById('request-success-text-field').innerHTML = "Thank you for signing up! An email has been sent to " + registerJSON.email + " with a " +
-                                                                          "confirmation link to activate your account. <a href='/login'>Click here</a> to return to the login page"
-        document.getElementById('register-div').hidden = true
-        requestSuccessDiv.hidden = false
-      }
-    ))
+      })
+      .catch((error) => {
+        console.error('Network error or unexpected issue:', error);
+      });
+    
   }
 
   return(
     <>
-      <div id="register-div" className="register-div">
+      {!isSuccess && (<div id="register-div" className="register-div">
         <h1 style={{color: "white"}}>Create account</h1>
 
         <form id="register-form" className="register-form">
@@ -67,22 +83,33 @@ const UserRegisterForm = () => {
           <hr />
 
 
-          <label htmlFor="email">Email:</label><br />
-          <input type="text" id="email" name="email" placeholder="Type your email"/><hr />
+          <label htmlFor="email">Email:</label>
+          <br />
+          {isSubmitted && (isEmailValid ? <FaCheckCircle style={{color: 'green'}}/> : <FaRegCircleXmark style={{color: 'red'}}/>)}
+          <input type="text" id="email" name="email" placeholder="Type your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <hr />
 
-          <label htmlFor="password">Password:</label><br />
-          <input type="password" id="password" name="password" placeholder="Type your password"/><hr />
+          <label htmlFor="password">Password:</label>
+          <br />
+          {isSubmitted && (isPasswordValid ? <FaCheckCircle style={{color: 'green'}}/> : <FaRegCircleXmark style={{color: 'red'}}/>)}
+          <input type="password" id="password" name="password" placeholder="Type your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <hr />
 
-          <label htmlFor="confirm-password">Confirm password:</label><br />
-          <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password"/><hr />
+          <label htmlFor="confirm-password">Confirm password:</label>
+          <br />
+          {isSubmitted && (isConfirmPasswordValid ? <FaCheckCircle style={{color: 'green'}}/> : <FaRegCircleXmark style={{color: 'red'}}/>)}
+          <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+          <hr />
         </form>
 
-        <button className="submit-button" id='submit-button' onClick={() => registrationHandler(window.document.getElementById('username').value, window.document.getElementById('password').value, window.document.getElementById('email').value, window.document.getElementById('confirm-password').value)}>SIGN UP</button>
-      </div>
+        <button className="submit-button" id='submit-button' onClick={() => registrationHandler()}>SIGN UP</button>
+      </div>)}
 
-      <div id="request-success-div" className="request-div" hidden={true}>
-        <p id="request-success-text-field"></p>
-      </div>
+      {isSuccess && (<div id="request-success-div" className="request-div">
+        <p id="request-success-text-field">
+          Thank you for signing up! An email has been sent to {email} with a confirmation link to activate your account. <a href="/login">Click here</a> to return to the login page.
+        </p>
+      </div>)}
     </>
   )
 }
