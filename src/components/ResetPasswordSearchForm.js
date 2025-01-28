c2194ee5ead81a06a7eb05c818500563957dc708
StrangeQuark <zacharyzirkle@hotmail.com>
2025-01-27 18:30:01 -0600

    Begin reformatting ResetPasswordSearchForm CSS

--- src/components/ResetPasswordSearchForm.js
+++ src/components/ResetPasswordSearchForm.js
@@ -35,19 +35,19 @@ const ResetPasswordSearchForm = () => {
 
     return(
         <>
-        {!isSuccess && (<div id="request-div" className="request-div">
+        {!isSuccess && (<div id="request-div" className="req-div">
             <h1 style={{color: "white"}}>Reset password</h1>
-            <div id="no-credentials-message-div" style={{height: "100px"}} >
-                {isError && (<b id="no-credentials-message" style={{color: "red"}}>Sorry, we could not find your account</b>)}
+            <div id="no-credentials-message-div" className="no-credentials-message-div" >
+                {isError && (<b id="no-credentials-message">Sorry, we could not find your account</b>)}
             </div>
-            <form id="request-form" className="request-form">
+            <form id="request-form" className="req-form">
                 <label htmlFor="credentials">Username or email:</label><br />
                 <input type="text" id="credentials" name="credentials" placeholder="Type your username or email" value={credentials} onChange={(e) => setCredentials(e.target.value)}/>
                 <hr />
             </form>
             <button className="submit-button" id='submit-button' onClick={() => requestHandler()}>SUBMIT</button>
         </div>)}
-        {isSuccess && (<div id="request-success-div" className="request-div">
+        {isSuccess && (<div id="request-success-div" className="req-div">
             <p id="request-success-text-field">Please check your email for a password reset link</p>
         </div>)}
         </>
