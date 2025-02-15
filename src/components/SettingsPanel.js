6e62a8e8622d1902d25c8f780a0800f1d5470ac9
StrangeQuark <zacharyzirkle@hotmail.com>
2025-02-14 19:13:28 -0600

    Update Delete profile button, needs more work

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -3,7 +3,20 @@ import { getRefreshToken } from "../utility/AuthUtility" /* Integration line: Au
 const SettingsPanel = () => {
 
     const deleteProfile = () => {
-        console.log(getRefreshToken())
+        const refreshToken = getRefreshToken()
+
+        var credentialsJson = { "credentials": "t", "password": "t" }
+
+        fetch('http://localhost:6001/deleteUser', {
+            method: "POST",
+            headers: {
+                'Authorization': "Bearer " + refreshToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentialsJson)
+        }).then(response => response.json().then((data) => {
+            console.log(data)
+        }))
     }
 
     return(
