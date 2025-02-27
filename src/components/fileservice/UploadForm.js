5a61aba559df6bae582ed197dc9a160d2c331492
StrangeQuark <zacharyzirkle@hotmail.com>
2025-02-27 11:38:35 -0600

    Remove unneeded semicolons

--- src/components/fileservice/UploadForm.js
+++ src/components/fileservice/UploadForm.js
@@ -2,29 +2,29 @@ import "./UploadForm.css"
 
 const UploadForm = () => {
     const handleFileUpload = async (event) => {
-        const file = event.target.files[0];
-        if (!file) return;
+        const file = event.target.files[0]
+        if (!file) return
 
-        const formData = new FormData();
-        formData.append("file", file);
+        const formData = new FormData()
+        formData.append("file", file)
 
         try {
             const response = await fetch("http://localhost:6010/upload", {
                 method: "POST",
                 body: formData,
-            });
-            const result = await response.text();
-            alert(result);
+            })
+            const result = await response.text()
+            alert(result)
         } catch (error) {
-            console.error("Upload failed", error);
+            console.error("Upload failed", error)
         }
-    };
+    }
 
     return(
         <div id="uploadForm" className="uploadForm">
             <input type="file" onChange={handleFileUpload} className="fileInput" />
         </div>
-    );
-};
+    )
+}
 
-export default UploadForm;
+export default UploadForm
