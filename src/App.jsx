1d11965d86e98bd6606d41402ab498a411d6f5a1
StrangeQuark <zacharyzirkle@hotmail.com>
2025-09-15 11:33:15 -0500

    Fix integration lines

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 import Login from './pages/Login' // Integration line: Auth
 import Register from './pages/Register' // Integration line: Auth
 import UserProfile from './pages/UserProfile' // Integration line: Auth
-import ResetPassword from './pages/ResetPassword' // Integration line: Email
+import ResetPassword from './pages/ResetPassword' // Integration line: Auth - Integration line: Email
 import ResetPasswordSearch from './pages/ResetPasswordSearch' // Integration line: Auth - Integration line: Email
 import ConfirmEmail from './pages/ConfirmEmail' // Integration line: Auth - Integration line: Email
 import Settings from './pages/Settings' // Integration line: Auth
@@ -17,7 +17,7 @@ const router = createBrowserRouter(
     { path: "/login", element: <Login /> }, // Integration line: Auth
     { path: "/register", element: <Register /> }, // Integration line: Auth
     { path: "/password-reset", element: <ResetPasswordSearch /> }, // Integration line: Auth - Integration line: Email
-    { path: "/new-password", element: <ResetPassword /> }, // Integration line: Email
+    { path: "/new-password", element: <ResetPassword /> }, // Integration line: Auth - Integration line: Email
     { path: "/user/:id", element: <UserProfile /> }, // Integration line: Auth - Integration line: Email
     { path: "/confirm-email", element: <ConfirmEmail /> }, // Integration line: Auth - Integration line: Email
     { path: "/settings", element: <Settings /> }, // Integration line: Auth
