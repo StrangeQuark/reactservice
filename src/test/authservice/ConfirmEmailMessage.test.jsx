4dec06a5d52ed80166dc380785082e8b66cecb96
StrangeQuark <zacharyzirkle@hotmail.com>
2025-11-28 21:55:39 -0600

    Fix broken unit tests

--- src/test/authservice/ConfirmEmailMessage.test.jsx
+++ src/test/authservice/ConfirmEmailMessage.test.jsx
@@ -5,10 +5,12 @@ import { render, screen, waitFor } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import { vi } from "vitest"
 import ConfirmEmailMessage from "../../components/authservice/ConfirmEmailMessage"
+import * as Telemetry from "../../utility/TelemetryUtility" // Integration line: Telemetry
 
 describe("ConfirmEmailMessage", () => {
   beforeEach(() => {
     vi.clearAllMocks()
+    vi.spyOn(Telemetry, "sendTelemetryEvent").mockImplementation(async () => {}) // Integration line: Telemetry
     global.fetch = vi.fn()
   })
 
