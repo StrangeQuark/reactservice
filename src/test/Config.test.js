a85a2820646603b0c2d08e4cb5eed45ec7f5ad94
StrangeQuark <zacharyzirkle@hotmail.com>
2026-09-22 08:31:08 -0500

    Add routing for the vpn service integration

--- src/test/Config.test.js
+++ src/test/Config.test.js
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, test, vi } from "vitest"
 
-const loadConfig = async (gatewayBaseUrl, gatewayIntegration = "true", authIntegration = "true") => {
+const loadConfig = async (gatewayBaseUrl, gatewayIntegration = "true", authIntegration = "true", vpnRouterIntegration = "false") => {
   vi.resetModules()
   vi.stubEnv("VITE_AUTH_API_BASE_URL", "http://auth-service:6001")
   vi.stubEnv("VITE_EMAIL_API_BASE_URL", "http://email-service:6005")
@@ -12,12 +12,14 @@ const loadConfig = async (gatewayBaseUrl, gatewayIntegration = "true", authInteg
   vi.stubEnv("VITE_FILESERVICE_INTEGRATION", "true")
   vi.stubEnv("VITE_VAULTSERVICE_INTEGRATION", "true")
   vi.stubEnv("VITE_GATEWAYSERVICE_INTEGRATION", gatewayIntegration)
+  vi.stubEnv("VITE_VPNROUTER_INTEGRATION", vpnRouterIntegration)
 
   return await import("../config")
 }
 
 afterEach(() => {
   vi.unstubAllEnvs()
+  vi.unstubAllGlobals()
 })
 
 describe("config", () => {
@@ -39,5 +41,19 @@ describe("config", () => {
     const { getAuthHeaders } = await loadConfig("", "false", "false")
 
     expect(getAuthHeaders("token")).toEqual({})
+  })
+
+  test("uses the VPN router origin when it is enabled", async () => {
+    vi.stubGlobal("window", {
+      location: {
+        hostname: "10.8.0.1",
+        origin: "http://10.8.0.1",
+        port: "",
+      },
+    })
+    const { AUTH_ENDPOINTS, FILE_ENDPOINTS } = await loadConfig("", "false", "true", "true")
+
+    expect(AUTH_ENDPOINTS.AUTHENTICATE).toBe("http://10.8.0.1/api/auth/authenticate")
+    expect(FILE_ENDPOINTS.GET_ALL).toBe("http://10.8.0.1/api/file/get-all")
   })
 })
